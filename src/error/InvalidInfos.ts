00be8d8571a26500b52f8b4375d9a6b0073fe1ba
Lucas Txai <txaifonceca@gmail.com>
2022-07-08 16:01:29 -0300

    (acho que) fiz o endpoint de pegar show por data

--- src/error/InvalidInfos.ts
+++ src/error/InvalidInfos.ts
@@ -1,19 +1,29 @@
 import { BaseError } from "./BaseError";
 
 export class invalidEmail extends BaseError {
-    constructor() {
-        super("E-mail inválido!", 400)
-    }
+  constructor() {
+    super("E-mail inválido!", 400);
+  }
 }
 
 export class invalidPassword extends BaseError {
-    constructor() {
-        super("Senha inválida!", 400)
-    }
+  constructor() {
+    super("Senha inválida!", 400);
+  }
 }
 
 export class invalidUserEmail extends BaseError {
-    constructor() {
-        super("E-mail já cadastrado!", 400)
-    }
+  constructor() {
+    super("E-mail já cadastrado!", 400);
+  }
+}
+export class invalidToken extends BaseError {
+  constructor() {
+    super("Token inválido!", 400);
+  }
+}
+export class invalidShowDate extends BaseError {
+  constructor() {
+    super("Nenhum show agendado para essa data!", 400);
+  }
 }
